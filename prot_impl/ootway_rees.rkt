b585226f81c6e82c4069ee342cf256a262cbb3fc
Siddhartha Singh <cs1221604@iitd.ac.in>
2025-02-14 22:25:06 +0530

    hand convert cspa file to forge for small test

--- prot_impl/ootway_rees.rkt
+++ prot_impl/ootway_rees.rkt
@@ -30,4 +30,5 @@
     (non-orig (ltk a s) (ltk b s))
     ; may need to add ltk's also here found an example where another person was able to generate the long term key
     (uniq-orig m na nb kab)
+    ; (try ltk a s)
 )
