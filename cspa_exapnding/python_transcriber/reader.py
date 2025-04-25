1fe81eb4606ade0a765554a9f60cf6e34cefc295
Siddhartha Singh <cs1221604@iitd.ac.in>
2025-04-25 15:19:06 +0530

    added some documentation to the reader file

--- cspa_exapnding/python_transcriber/reader.py
+++ cspa_exapnding/python_transcriber/reader.py
@@ -127,6 +127,7 @@ class Role:
     def __repr__(self):
         return self.__str__()
 class Protocol:
+    """Stores all the roles involved and the protocol name itself"""
     def __init__(self,roles_arr:List[Role],prot_name:str,basic_str:str):
         self.roles_arr = roles_arr
         self.prot_name = prot_name
@@ -143,6 +144,7 @@ class Protocol:
     def __repr__(self):
         return self.__str__()
 class Strand:
+    """Stores the variable mapping between role and skeleton specified in defstrand clause"""
     #i think trace len is being ignored for now?
     def __init__(self,role_name:str,trace_len:int,var_map:Dict[str,str]):
         self.role_name = role_name
@@ -153,9 +155,11 @@ class Strand:
     def __repr__(self):
         return self.__str__()
 class ConstrType(Enum):
+    """Enum for types of constraints"""
     UNIQ_ORIG = 0
     NON_ORIG = 1
 class Constraint:
+    """Refers to an individual constraint of type either non-orig/uniq-orig refering to some message"""
     def __init__(self,constr_type:ConstrType,msg_on_constr:Message):
         self.constr_type = constr_type
         self.msg_on_constr = msg_on_constr
@@ -164,6 +168,7 @@ class Constraint:
     def __repr__(self):
         return self.__str__()
 class Skeleton:
+    """Stores all the constraints and variable map defined in the defskeleton clause"""
     def __init__(self,prot_name:str,var_dict:Dict[str,Variable],strand_list:List[Strand],orig_constr:List[Constraint]):
         self.prot_name = prot_name
         self.var_dict = var_dict
@@ -174,10 +179,13 @@ class Skeleton:
     def __repr__(self):
         return self.__str__()
 class ParseException(Exception):
+    """parse exception used to signal an exception in lot of the transcribing functions"""
     def __init__(self,message):
         self.message = message
         super().__init__(message)
 
+"""list of strings appearing in CPSA syntax collected here so prevent 
+repetion and avoid inconsistencies"""
 DEF_PROT_STR = "defprotocol"
 DEF_SKEL_STR = "defskeleton"
 DEF_STRAND_STR = "defstrand"
@@ -196,13 +204,17 @@ PUBK_STR = "pubk"
 PRIVK_STR = "privk"
 
 def s_expr_instead_of_str(expected_str:str,s_expr) -> ParseException:
+    """function to generate a common exception where there should have been a simple string but found an s-expression instead"""
     return ParseException(f"Expected '{expected_str}' string not an s expression {str(s_expr)} with type {type(s_expr)}")
 
 def unexpected_str_error(expected_str:str,unexpected_str:str) -> ParseException:
+    """function to generate a common exception where the s_expr contains an unexpected string 
+    Ex: expected defprotocol at beginning of protocol but saw something else"""
     return ParseException(f"Expected '{expected_str}' string at beginning of s expression not {unexpected_str}")
 
 
 def get_var(var_name_str:str,variable_dict:Dict[str,Variable]) -> Variable:
+    """returns the variable corresponding to a variable name, if not present raise an exception"""
     if var_name_str not in variable_dict:
         raise ParseException(f"Unknown Variable Name {var_name_str} known variables are {variable_dict.keys()}")
     return variable_dict[var_name_str]
@@ -211,6 +223,7 @@ def is_symbol_type(symbl) -> bool:
     return type(symbl) == sexpdata.Symbol
 
 def match_type_and_str(s_expr,expected_str:str) -> None:
+    """helper function to check that an s-expr is a specific string like defprotocol"""
     if not is_symbol_type(s_expr):
         raise s_expr_instead_of_str(expected_str,s_expr)
     if str(s_expr) != expected_str:
@@ -218,18 +231,24 @@ def match_type_and_str(s_expr,expected_str:str) -> None:
 
 
 def match_var_type(var:Variable,expected_var_type:VarType) -> None:
+    """generates exception when a variable is not a correct type, 
+    example when encrypting a key cannot be a  name"""
     if var.var_type != expected_var_type:
         raise ParseException(f"Expected var type = {vartype_to_str(expected_var_type)} but got {vartype_to_str(var.var_type)}")
 
 def get_str_from_symbol(s_expr:sexpdata.Symbol,data_name:str) -> str:
+    """simply gets the string from an s-expr"""
     if not is_symbol_type(s_expr):
         raise s_expr_instead_of_str(data_name,s_expr)
     return str(s_expr)
 def get_int_from_s_expr(s_expr,data_name:str) -> str:
+    """simply gets the int from an s-expr"""
     if type(s_expr) != int:
         raise ParseException(f"Expected type int here not type {type(s_expr)}")
     return s_expr
 def parse_vars_list(s_expr,var_dict) -> None:
+    """this functions parses a list of variables parses expressions 
+    like (a b name) found inside a vars clause"""
     if len(s_expr) < 2:
         raise ParseException(f"Expected variable name and type not {str(s_expr)}")
     data_type_str = get_str_from_symbol(s_expr[-1],"variable type")
@@ -242,6 +261,8 @@ def parse_vars_list(s_expr,var_dict) -> None:
         var_dict[cur_var_str] = cur_var 
     
 def parse_vars_clause(s_expr) -> Dict[str,Variable]:
+    """this function parses the vars clause of the 
+    form (vars (a b name) (n1 n2 text))"""
     var_dict = {}
     if len(s_expr) < 2:
         raise ParseException(f"Expected 'vars' and variables list in the s expression but length of s expression is = {len(s_expr)}")
@@ -252,6 +273,8 @@ def parse_vars_clause(s_expr) -> Dict[str,Variable]:
 
 #parses encrypted term and cat term for now
 def parse_term(s_expr,var_dict:Dict[str,Variable]) -> Message:
+    """this function parses a message term, either just a base term 
+    like a variable name, ex n1 in (enc n1 (pubk b))"""
     if type(s_expr) == sexpdata.Symbol:
         var_name = get_str_from_symbol(s_expr,"var name")
         var = get_var(var_name,var_dict)
@@ -260,6 +283,7 @@ def parse_term(s_expr,var_dict:Dict[str,Variable]) -> Message:
         raise ParseException(f"empty s-expr cannot be a term")
     
 def parse_ltk(s_expr,var_dict:Dict[str,Variable]) -> Message:
+    """this function parses the ltk clause of the form (ltk name1 name2)"""
     if len(s_expr) != 3:
         raise ParseException(f"Expected s-expr of length 3 ltk name1 name2 but length is {len(s_expr)}")
     match_type_and_str(s_expr[0],LTK_STR)
@@ -276,6 +300,7 @@ def parse_ltk(s_expr,var_dict:Dict[str,Variable]) -> Message:
     return Message(MsgType.LTK_TERM,[name_1_term,name_2_term])
 
 def parse_pubk(s_expr,var_dict:Dict[str,Variable]) -> Message:
+    """this function parses the pubk clause of the form (pubk name1)"""
     if len(s_expr) != 2:
         raise ParseException(f"Expected s-expr of length 2 pibk name but length is {len(s_expr)} s_expr is {str(s_expr)}")
     match_type_and_str(s_expr[0],PUBK_STR)
@@ -286,6 +311,7 @@ def parse_pubk(s_expr,var_dict:Dict[str,Variable]) -> Message:
     name_term = Message(MsgType.ATOM_TERM,name)
     return Message(MsgType.PUBK_TERM,[name_term])
 def parse_privk(s_expr,var_dict:Dict[str,Variable]) -> Message:
+    """this function parses the privk clause of the form (privk name1) """
     if len(s_expr) != 2:
         raise ParseException(f"Expected s-expr of length 2 privk, name but length is {len(s_expr)} s_expr is {str(s_expr)}")
     match_type_and_str(s_expr[0],PRIVK_STR)
@@ -296,12 +322,14 @@ def parse_privk(s_expr,var_dict:Dict[str,Variable]) -> Message:
     name_term = Message(MsgType.ATOM_TERM,name)
     return Message(MsgType.PRIVK_TERM,[name_term])    
 def is_valid_key(msg:Message) -> bool:
+    """checks if a message term is a valid key, needed to check if can encrypt using this"""
     if msg.msg_type in [MsgType.PUBK_TERM,MsgType.LTK_TERM]:
         return True
     if msg.msg_type == MsgType.ATOM_TERM and msg.msg_data.var_type in [VarType.AKEY,VarType.SKEY]:
         return True
     return False
 def parse_enc_term(s_expr,var_dict:Dict[str,Variable]) -> Message:
+    """parses an encrypted term like (enc n1 n2 (pubk a))"""
     if len(s_expr) < 3:
         raise ParseException(f"Expected atleast three terms enc,message and encryption key")
     data_terms_lst = [parse_term(t,var_dict) for t in s_expr[1:-1]]
@@ -312,20 +340,26 @@ def parse_enc_term(s_expr,var_dict:Dict[str,Variable]) -> Message:
     data_terms_lst.append(key_term)
     return Message(MsgType.ENCRYPTED_TERM,data_terms_lst)
 
-##condenses any nested cat term not used any example yet will impl later
+#condenses any nested cat term not used any example yet will impl later
 def condense_cat_term(msg_list:List[Message]) -> Message:
+    """TODO: would condednse nested cat terms to use only one concatenated messages
+    exampke (cat n1 (cat n2 n3)) == (cat n1 n2 n3)"""
     pass
 def parse_cat_term(s_expr,var_dict:Dict[str,Variable]) -> Message:
+    """parses concatenated terms like (cat a (enc n1 n2 (pubk a))"""
     if len(s_expr) < 1:
         raise ParseException(f"Expected atleast two terms cat and some data")
     data_terms_lst = [parse_term(t,var_dict) for t in s_expr[1:]]
     return Message(MsgType.CAT_TERM,data_terms_lst)
 
 def parse_atom(s_expr:sexpdata.Symbol,var_dict:Dict[str,Variable]) -> Variable:
+    """parses an atomic term, consisting of just one variable like 
+    n1 where n1 is a variable defined in the vars clause of a role"""
     var_term_name = get_str_from_symbol(s_expr,"message term")
     var_term = get_var(var_term_name,var_dict)
     return var_term
 def parse_term(s_expr,var_dict:Dict[str,Variable]) -> Message:
+    """parses any term of either encrypted term,concatenated term,ltk term,pubk term etc."""
     if type(s_expr) == sexpdata.Symbol:
         var_term = parse_atom(s_expr,var_dict)
         return Message(MsgType.ATOM_TERM,var_term)
@@ -345,6 +379,8 @@ def parse_term(s_expr,var_dict:Dict[str,Variable]) -> Message:
     return func_dict[enc_cat_ltk_pubk_str](s_expr,var_dict)
 
 def parse_indv_trace(s_expr,var_dict) -> Tuple[SendRecv,Message]:
+    """parses individual trace present in the trace clause of a role 
+    Example: (send (enc (n1 n2 (pubk a))))"""
     if len(s_expr) != 2:
         raise ParseException(f"Expected send/recv followed by message as s_expr len(s_expr) is: {len(s_expr)}")
     send_or_recv_str = get_str_from_symbol(s_expr[0],"send/recv")    
@@ -356,6 +392,8 @@ def parse_indv_trace(s_expr,var_dict) -> Tuple[SendRecv,Message]:
     return trace_type,parse_term(s_expr[1],var_dict)
 
 def parse_trace(s_expr,var_dict) -> List[Tuple[SendRecv,Message]]:
+    """parses the trace in a role consisting of a sequence of sends and recieves
+    Example : (trace (send (enc n1 (pubk b))) (recv (enc n1 n2 (pubk a))) (send (enc n2 (pubk b))))"""
     #currently forcing limit of 3 becuase the current racket code seems 
     #to have that restriction as well will see what to do with this later
     if len(s_expr) < 3:
@@ -367,6 +405,8 @@ def parse_trace(s_expr,var_dict) -> List[Tuple[SendRecv,Message]]:
     return result
 
 def parse_role(s_expr) -> Role:
+    """parses the role clause present inside a protocol clause 
+    Example:(defrole role_name (vars ..) ( trace .. ))"""
     if len(s_expr) != 4:
         raise ParseException(f"Expected 'defrole',role_name,variables list and trace of sends and receives but length of s expr is {len(s_expr)}")
     match_type_and_str(s_expr[0],DEF_ROLE_STR)
@@ -377,6 +417,8 @@ def parse_role(s_expr) -> Role:
     return Role(role_name,var_dict,msg_trace)    
 
 def parse_protocol(s_expr) -> Protocol:
+    """parses the whole protocol clause which includes multiple role clauses in it 
+    (defprotocol prot_name basic (defrole .. ) (defrole .. ))"""
     if len(s_expr) < 4:
         raise ParseException(f"Expected 'defprotocol',protocol name,'basic' and atleast one role but size of s_expr is = {len(s_expr)}")
     match_type_and_str(s_expr[0],DEF_PROT_STR)
@@ -407,10 +449,13 @@ def parse_skeleton(s_expr):
             constr_arr.append(parse_uniq_orig(sub_s_expr))
 """
 def parse_var_mapping(s_expr,var_map_dict:Dict[str,str],role_obj:Role):
+    """parses the variable mapping in a defstrand clause example (a b) where 
+    a is a variable in the role b is a variable in the skeleton,function also 
+    checks that the variable names are valid"""
     if len(s_expr) != 2:
         raise ParseException("Expected skeleton variable name,role variable name")
     skelet_var_name = get_str_from_symbol(s_expr[0],"skeleton var name")
-    ##should add check for if role names and variable names are valid as well
+    ##should add check for if role names and variable names re valid as well
     role_var_name = get_str_from_symbol(s_expr[1],"role var name")
     if not role_obj.is_valid_var_name(role_var_name):
         raise ParseException(f"Varible name '{role_var_name}' and type '{type(role_var_name)}' not present in role declaration '{role_obj.role_name}' which has variable list '{list(role_obj.var_dict.keys())}' '{list(map(type,list(role_obj.var_dict.keys())))}'")
@@ -418,6 +463,7 @@ def parse_var_mapping(s_expr,var_map_dict:Dict[str,str],role_obj:Role):
         raise ParseException(f"Repeated variable name {skelet_var_name} in defstrand clause")
     var_map_dict[skelet_var_name] = role_var_name
 def parse_strand(s_expr,prot_obj:Protocol) -> Strand:
+    """parses the strand clause of the form (defstrand role_name trace_len (var1 var2) ...)"""
     if len(s_expr) < 4:
         raise ParseException("Expected defstrand,role_name,strand len?,variable mappings")
     match_type_and_str(s_expr[0],DEF_STRAND_STR)
@@ -435,6 +481,9 @@ def parse_strand(s_expr,prot_obj:Protocol) -> Strand:
 #only handles parsing atom terms,pubk,privk,ltk as examples
 #doesn't handle parsing cat,enc
 def parse_base_term(s_expr,var_dict:Dict[str,Variable]) -> Message:
+    """parses only base terms like a variable name,public key,private key,
+    long term key. Not concatenated or encrypted terms, used to restrict what
+    terms are used for non-orig and uniq-orig"""
     if type(s_expr) == sexpdata.Symbol:
         var_term = parse_atom(s_expr,var_dict)  
         return Message(MsgType.ATOM_TERM,var_term)
@@ -450,6 +499,7 @@ def parse_base_term(s_expr,var_dict:Dict[str,Variable]) -> Message:
     else:
         raise ParseException(f"Unexpected clause_type {clause_type} expected pubk/privk/ltk")
 def parse_non_orig(s_expr,var_dict:Dict[str,Variable],constr_lst:List[Constraint]):
+    """parse non orig clause of the form (non-orig term1 term2 ...)"""
     if len(s_expr) < 2:
         raise ParseException("expected non-orig and variable clause")
     match_type_and_str(s_expr[0],NON_ORIG_STR)
@@ -459,6 +509,7 @@ def parse_non_orig(s_expr,var_dict:Dict[str,Variable],constr_lst:List[Constraint
         constr_lst.append(cur_constr)
 
 def parse_uniq_orig(s_expr,var_dict:Dict[str,Variable],constr_list:List[Constraint]):
+    """parse uniq-orig clause of the form (uniq-orig term1 term2 ...)"""
     if len(s_expr) < 2:
         raise ParseException("expected non-orig and variable clause")
     match_type_and_str(s_expr[0],UNIQ_ORIG_STR)
@@ -468,6 +519,9 @@ def parse_uniq_orig(s_expr,var_dict:Dict[str,Variable],constr_list:List[Constrai
         constr_list.append(cur_constr)
 
 def parse_skeleton(s_expr,prot_obj:Protocol) -> Skeleton:
+    """parses a skeleton clause containing the variable declrations, strand declarations,
+    non-orig and uniq-orig constraints 
+    Ex: (defskeleton (vars ...) (defstrand ...) (non-orig ..) ..)"""
     if type(s_expr) == sexpdata.Symbol:
         raise ParseException("Expected s expression not string literal")
     if len(s_expr) == 1:
@@ -493,6 +547,8 @@ def parse_skeleton(s_expr,prot_obj:Protocol) -> Skeleton:
     return Skeleton(prot_name,vars_dict,strand_arr,constraints_lst)
 ##TODO: Just wrote parse_skeleton function also have to test it
 def parse_file(s_expr):
+    """helper function to handle the cases where the file contains only protocol clause
+    or protocol and skeleton clause"""
     if len(s_expr) == 0:
         raise ParseException("Empty S expression expected defprotocol clause")
     if len(s_expr) == 1:
