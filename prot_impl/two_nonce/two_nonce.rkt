afe12ce63375c1401984967e12d26499960b4bae
Siddhartha Singh <sidsiskilled@gmail.com>
2026-04-21 13:30:20 +0530

    allows running partial traces now tested with two_nonce

--- prot_impl/two_nonce/two_nonce.rkt
+++ prot_impl/two_nonce/two_nonce.rkt
@@ -45,6 +45,16 @@
   (init 1) (resp 1)
   )
 
+(defaltinstance partial_single_session
+  (Timeslot 5)
+  (mesg 33)
+  (Key 6) (name 3) (Ciphertext 10) (text 6) (tuple 8) (Hashed 0) ;; in tuple model Hashed has to be 0 currently as only storing hashed model
+  (akey 6) (skey 0) (Attacker 1)
+  (PublicKey 3) (PrivateKey 3)
+  (enc-depth 2) (tuple-length 2)
+  (init 1) (resp 1)
+  )
+
 (defaltinstance alt_double_session
   (Timeslot 12)
   (mesg 49)
